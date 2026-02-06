cc5545714886cd08dd28403b677eab46428822bb
omofolarin <folarinshonibare@yahoo.com>
2026-02-06 08:36:15 +0100

    Initial Commit

---
+++ ShortVideoFeed/ShortVideoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortVideoFeedApp.swift
+//  ShortVideoFeed
+//
+//  Created by Shonibare Omofolarin Ajibade on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShortVideoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShortVideoFeedTests/ShortVideoFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShortVideoFeedTests.swift
+//  ShortVideoFeedTests
+//
+//  Created by Shonibare Omofolarin Ajibade on 06/02/2026.
+//
+
+import Testing
+@testable import ShortVideoFeed
+
+struct ShortVideoFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
